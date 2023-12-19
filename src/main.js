4c377093c28172315634abdb3b4a1c48fd2607ee
antoniodalterio <antoniodalterio2227@gmail.com>
2023-12-19 13:34:33 +0100

    feat: install FontAwesome

--- src/main.js
+++ src/main.js
@@ -2,4 +2,15 @@ import { createApp } from 'vue';
 import './assets/scss/style.scss';
 import App from './App.vue';
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+/* import specific icons */
+
+/* add icons to the library */
+library.add().component('font-awesome-icon', FontAwesomeIcon);
+
 createApp(App).mount('#app');
