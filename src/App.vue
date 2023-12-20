28e7e2529876ba9de38c258f25ff4f4ee348a055
antoniodalterio <antoniodalterio2227@gmail.com>
2023-12-20 23:03:38 +0100

    feat: MainInApp

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <script>
 import HeaderApp from './components/HeaderApp.vue';
+import MainInApp from './components/MainInApp.vue';
 export default {
   components: {
     HeaderApp,
+    MainInApp,
   },
 };
 </script>
 
 <template>
   <HeaderApp></HeaderApp>
+  <MainInApp></MainInApp>
 </template>
-
-<style></style>
