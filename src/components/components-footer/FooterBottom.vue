0ef9fa118a9ef7b95d41315bfc7b9d7570c3a094
antoniodalterio <antoniodalterio2227@gmail.com>
2023-12-21 00:00:55 +0100

    feat: FooterBootm

--- src/components/components-footer/FooterBottom.vue
+++ src/components/components-footer/FooterBottom.vue
@@ -3,4 +3,28 @@ export default {
   name: 'FooterBottom',
 };
 </script>
-<template></template>
+<template>
+  <footer class="bg-dark text-white py-5">
+    <div class="ad-container d-flex justify-content-between align-items-center">
+      <p class="m-0">&#169;2020 phlox buisness theme.</p>
+      <!-- ul col 2 socials -->
+      <ul class="d-flex m-0 gap-2">
+        <!-- facebook -->
+        <li class="list-group">
+          <a href="#"><font-awesome-icon icon="fa-brands fa-facebook-f" /></a>
+        </li>
+
+        <!-- x-Twitter -->
+        <li class="list-group">
+          <a href="#"><font-awesome-icon icon="fa-brands fa-x-twitter" /></a>
+        </li>
+
+        <!-- pinterest -->
+        <li class="list-group">
+          <a href="#"><font-awesome-icon icon="fa-brands fa-pinterest-p" /></a>
+        </li>
+      </ul>
+      <!-- ul col 2 socials -->
+    </div>
+  </footer>
+</template>
